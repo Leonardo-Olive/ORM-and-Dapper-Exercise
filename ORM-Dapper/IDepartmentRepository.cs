f96b17b5c544190bb385da02ac9b41d7ce70364e
leonardo oliveira <leonardo_olivesilva@icloud.com>
2025-07-28 18:06:08 +0200

    ORM exercice 1 and 2

---
+++ ORM-Dapper/IDepartmentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ORM_Dapper
+{
+    public interface IDepartmentRepository
+    {
+        public IEnumerable<Department> GetAllDepartments();
+    }
+}
+
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperDepartmentRepository(conn);
+            var departments = repo.GetAllDepartments();
+
+            Console.WriteLine("Would you like to enter:");
+            Console.WriteLine("1 - Departments");
+            Console.WriteLine("2 - Products");
+
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            if (input == 1)
+            {
+
+
+                foreach (var dept in departments)
+                {
+                    Console.Write($"{dept.DepartmentID} {dept.Name}");
+                   
+                }
+
+                
+            }
+
+           var repos = new DapperProductRepository(conn);
+           // Console.WriteLine("What is the name of your new product?");
+           // var prodName = Console.ReadLine();
+
+           // Console.WriteLine("What is the price?");
+           // var prodPrice = double.Parse(Console.ReadLine());
+
+          //  Console.WriteLine("What is the category ID");
+           // var prodCar = int.Parse(Console.ReadLine());
+
+           // repos.CreateProduct(prodName, prodPrice, prodCar);
+            var prodList = repos.GetAllProducts();
+
+            if (input == 2)
+            {
+                foreach (var prod in prodList)
+                {
+                    Console.WriteLine($"{prod.ProductID} - {prod.Name}");
+                    
+                }
+            }
+          
+  
+  
         }
+
+
     }
 }
